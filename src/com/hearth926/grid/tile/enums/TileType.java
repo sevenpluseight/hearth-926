133f27feca9049d4f78d254f3c5606e8b3a04fdf
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-12-08 12:12:10 +0800

    feat: add tile enum for texture

---
+++ src/com/hearth926/grid/tile/enums/TileType.java
@@ -0,0 +1,31 @@
+package com.hearth926.grid.tile.enums;
+
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+
+import java.util.Objects;
+
+public enum TileType {
+    GRASS(Color.LIGHTGREEN, "/images/tiles/grass.png"),
+    DIRT(Color.SADDLEBROWN, "/images/tiles/dirt.png"),
+    WATER(Color.DEEPSKYBLUE, "/images/tiles/water.png");
+
+    private final Color color;
+    private final Image image;
+
+    TileType(Color color, String resourcePath) {
+        this.color = color;
+        // Load from resources folder
+        Image img;
+        try {
+            img = new Image(Objects.requireNonNull(getClass().getResourceAsStream(resourcePath)));
+        } catch (Exception e) {
+            System.out.println("Failed to load image: " + resourcePath);
+            img = null;
+        }
+        this.image = img;
+    }
+
+    public Color getColor() { return color; }
+    public Image getImage() { return image; }
+}
