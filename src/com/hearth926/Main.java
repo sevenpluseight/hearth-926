c35589ca6493824a32d16eb5dd590aa9ea9434ea
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-12-08 21:42:47 +0800

    feat: make game window full-screen based on device resolution

--- src/com/hearth926/Main.java
+++ src/com/hearth926/Main.java
@@ -2,18 +2,28 @@ package com.hearth926;
 
 import com.hearth926.game.GameScene;
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        double width = 800;
-        double height = 800;
+
+        Rectangle2D screen = Screen.getPrimary().getVisualBounds();
+
+        double width = screen.getWidth();
+        double height = screen.getHeight();
 
         GameScene gameScene = new GameScene(width, height);
 
         stage.setTitle("Hearth:926");
         stage.setScene(gameScene.getScene());
+        stage.setX(screen.getMinX());
+        stage.setY(screen.getMinY());
+        stage.setWidth(width);
+        stage.setHeight(height);
+
         stage.show();
     }
 
