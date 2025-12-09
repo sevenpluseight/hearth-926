c98c9cea4e7d047d473c73bc073d18eb35a58f8c
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-12-09 23:17:20 +0800

    feat: added smooth animation of player direction walking

--- src/com/hearth926/player/Player.java
+++ src/com/hearth926/player/Player.java
@@ -1,61 +1,134 @@
 package com.hearth926.player;
 
 import com.hearth926.grid.Grid;
+import com.hearth926.player.enums.Direction;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class Player {
     private int row;
     private int col;
+    private boolean moving = false;
     private final Rectangle node;
     private final Grid grid;
-    private final double size = 15;
+    private static final double SIZE = 15;
+    private static final Duration MOVE_TIME = Duration.millis(180);
 
     public Player(int row, int col, Grid grid) {
         this.row = row;
         this.col = col;
         this.grid = grid;
-        this.node = new Rectangle(size, size, Color.RED);
-        updatePosition();
+//        this.node = new Rectangle (size, size, Color.RED);
+
+        node = new Rectangle(SIZE, SIZE, Color.RED);
+//        updatePosition();
+        setInstantPosition();
     }
 
-    private void updatePosition() {
+//    private void updatePosition() {
+//        double xOffset = grid.getSceneWidth() / 2;
+//        double yOffset = grid.getSceneHeight() / 2 - (grid.getRows() + grid.getCols()) * grid.getTileHeight() / 4;
+//
+//        double x = (col - row) * grid.getTileWidth() / 2 + xOffset - size / 2;
+//        double y = (col + row) * grid.getTileHeight() / 2 + yOffset - size / 2;
+//
+//        node.setTranslateX(x);
+//        node.setTranslateY(y);
+//    }
+//
+//    public void moveUp() {
+//        if (row > 0) {
+//            row--;
+//            updatePosition();
+//        }
+//    }
+//
+//    public void moveDown() {
+//        if (row < grid.getRows() - 1 && (row + col) < grid.getRows() + grid.getCols() - 2) {
+//            row++;
+//            updatePosition();
+//        }
+//    }
+//
+//    public void moveLeft() {
+//        if (col > 0) {
+//            col--;
+//            updatePosition();
+//        }
+//    }
+//
+//    public void moveRight() {
+//        if (col < grid.getCols() - 1 && (row + col) < grid.getRows() + grid.getCols() - 2) {
+//            col++;
+//            updatePosition();
+//        }
+//    }
+//
+//    public Rectangle getNode() {
+//        return node;
+//    }
+
+    // Initial placement
+    private void setInstantPosition() {
+        node.setTranslateX(calculateX(col, row));
+        node.setTranslateY(calculateY(col, row));
+    }
+
+    private double calculateX(int col, int row) {
         double xOffset = grid.getSceneWidth() / 2;
+        return (col - row) * grid.getTileWidth() / 2 + xOffset - SIZE / 2;
+    }
+
+    private double calculateY(int col, int row) {
         double yOffset = grid.getSceneHeight() / 2 - (grid.getRows() + grid.getCols()) * grid.getTileHeight() / 4;
-
-        double x = (col - row) * grid.getTileWidth() / 2 + xOffset - size / 2;
-        double y = (col + row) * grid.getTileHeight() / 2 + yOffset - size / 2;
-
-        node.setTranslateX(x);
-        node.setTranslateY(y);
+        return (col + row) * grid.getTileHeight() / 2 + yOffset - SIZE / 2;
     }
 
-    public void moveUp() {
-        if (row > 0) {
-            row--;
-            updatePosition();
+    // Public controls
+    public void move(Direction direction) {
+        if (moving) return;
+
+        int targetRow = row;
+        int targetCol = col;
+
+        switch (direction) {
+            case UP -> targetRow--;
+            case DOWN -> targetRow++;
+            case LEFT -> targetCol--;
+            case RIGHT -> targetCol++;
         }
+
+        if (!isValid(targetRow, targetCol)) return;
+
+        animateMove(targetRow, targetCol);
     }
 
-    public void moveDown() {
-        if (row < grid.getRows() - 1 && (row + col) < grid.getRows() + grid.getCols() - 2) {
-            row++;
-            updatePosition();
-        }
+    // Core logic
+    private void animateMove(int newRow, int newCol) {
+        moving = true;
+
+        double newX = calculateX(newCol, newRow);
+        double newY = calculateY(newCol, newRow);
+
+        TranslateTransition move = new TranslateTransition(MOVE_TIME, node);
+        move.setToX(newX);
+        move.setToY(newY);
+        move.setInterpolator(Interpolator.EASE_BOTH);
+
+        move.setOnFinished(e -> {
+            row = newRow;
+            col = newCol;
+            moving = false;
+        });
+
+        move.play();
     }
 
-    public void moveLeft() {
-        if (col > 0) {
-            col--;
-            updatePosition();
-        }
-    }
-
-    public void moveRight() {
-        if (col < grid.getCols() - 1 && (row + col) < grid.getRows() + grid.getCols() - 2) {
-            col++;
-            updatePosition();
-        }
+    private boolean isValid(int row, int col) {
+        return row >= 0 && col >= 0 && row < grid.getRows() && col < grid.getCols();
     }
 
     public Rectangle getNode() {
