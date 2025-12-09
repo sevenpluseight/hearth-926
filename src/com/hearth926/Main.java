a3978c62be761b5e0f6723c7f301bd9f56d75d2b
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-12-09 17:34:53 +0800

    feat: added TitleScene

--- src/com/hearth926/Main.java
+++ src/com/hearth926/Main.java
@@ -1,6 +1,7 @@
 package com.hearth926;
 
 import com.hearth926.game.GameScene;
+import com.hearth926.game.TitleScene;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
@@ -9,21 +10,22 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-
         Rectangle2D screen = Screen.getPrimary().getVisualBounds();
-
         double width = screen.getWidth();
         double height = screen.getHeight();
 
-        GameScene gameScene = new GameScene(width, height);
+        TitleScene titleScene = new TitleScene(width, height, () -> {
+            GameScene gameScene = new GameScene(width, height);
+            stage.setScene(gameScene.getScene());
+        });
 
         stage.setTitle("Hearth:926");
-        stage.setScene(gameScene.getScene());
+//        stage.setScene(gameScene.getScene());
         stage.setX(screen.getMinX());
         stage.setY(screen.getMinY());
         stage.setWidth(width);
         stage.setHeight(height);
-
+        stage.setScene(titleScene.getScene());
         stage.show();
     }
 
