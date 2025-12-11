982bd06d0f60f2aefabbaf61d1b83ff9f8da53b5
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-12-11 22:53:50 +0800

    feat: added color for the different role of NPCs

--- src/com/hearth926/npc/NPCType.java
+++ src/com/hearth926/npc/NPCType.java
@@ -1,5 +1,19 @@
 package com.hearth926.npc;
 
+import javafx.scene.paint.Color;
+
 public enum NPCType {
-    SIDE_CHARACTER, SHOPKEEPER, GUIDE;
+    SIDE_CHARACTER(Color.DARKBLUE),
+    SHOPKEEPER(Color.GOLD),
+    GUIDE(Color.DARKGREEN);
+
+    private final Color color;
+
+    NPCType(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
